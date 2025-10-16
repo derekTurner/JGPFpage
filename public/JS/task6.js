4ca7ce7de2505461bc41afabcd20169ceff3c4b1
derekTurner <derek.turner@uws.ac.uk>
2025-10-16 12:02:06 +0100

    week5
    roll dice

---
+++ public/JS/task6.js
@@ -0,0 +1,10 @@
+function totup(myForm){
+let total = 0;
+for(let i = 0; i < myForm.elements.entry.length; i ++){
+    
+    total += parseFloat(myForm.elements.entry[i].value)}
+console.log("total",total )
+total = total * (1 - (myForm.elements.d1.value/100))
+
+document.getElementById("totalled").innerHTML = total.toFixed(2);
+}
